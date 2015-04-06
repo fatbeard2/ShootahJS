566ebed0b04aa3557de468d2d79a9644b6d1f782
andrey.chernyshev <om08uhkn@gmail.com>
2015-04-06 17:01:07 +0300

    Added grunt task to copy common modules

---
+++ gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        jshint: {
+            files: [
+                '**/*.js',
+                '!**/client/bower_components/**',
+                '!**/node_modules/**'
+            ],
+            options: {
+                globals: {
+                    define: true
+                }
+            }
+        },
+        shell: {
+            bower_install: {
+                command: 'bower install'
+            },
+            npm_install: {
+                command: 'npm install'
+            }
+        },
+        copy: {
+            common: {
+                expand: true,
+                cwd: 'client/bower_components',
+                src: 'physicsjs/**',
+                dest: 'common/libs/'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-shell');
+
+    grunt.registerTask('default', ['shell:bower_install', 'shell:npm_install', 'copy:common']);
+
+};
